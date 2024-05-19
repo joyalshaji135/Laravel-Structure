34cac76e325da8e238da94018266cce118753b45
joyalshaji135 <joyalshaji135@gmail.com>
2024-05-19 22:22:43 +0530

    Laravel Basic Structure

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function(){
+    return view('about');
+
+});
